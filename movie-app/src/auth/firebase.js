a43906dc8fbd0f5408b4c6d84aed511a410cb3d9
ShamaniCoder <ertugrulaktn@gmail.com>
2022-05-14 23:00:53 +0200

    added .env and completed firebase

--- movie-app/src/auth/firebase.js
+++ movie-app/src/auth/firebase.js
@@ -2,30 +2,35 @@ import { initializeApp } from "firebase/app";
 import {
   createUserWithEmailAndPassword,
   getAuth,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
   signOut,
+  updateProfile,
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCX8fNzvrv9Wi6U6wsEZttJeQYHubgMBko",
-  authDomain: "movie-app-2c8e5.firebaseapp.com",
-  projectId: "movie-app-2c8e5",
-  storageBucket: "movie-app-2c8e5.appspot.com",
-  messagingSenderId: "569734264447",
-  appId: "1:569734264447:web:f9084ae1f209037768314a",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
-export const createUser = async (email, password, navigate) => {
+export const createUser = async (email, password, displayName, navigate) => {
   try {
     let userCredential = await createUserWithEmailAndPassword(
       auth,
       email,
       password
     );
+    await updateProfile(auth.currentUser, {
+      displayName: displayName,
+    })
     console.log(userCredential);
     navigate("/");
   } catch (err) {
@@ -51,3 +56,13 @@ export const logOut = () => {
   signOut(auth);
   alert("log out successfully");
 };
+
+export const userObserver = (setCurrentUser) => {
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) {
+      setCurrentUser(currentUser);
+    } else {
+      setCurrentUser(false)
+    }
+  });
+}
