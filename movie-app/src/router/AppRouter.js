8c06f07ab13721214a8369ddb9811d773c1b25c2
ShamaniCoder <ertugrulaktn@gmail.com>
2022-05-17 01:10:15 +0200

    added VideoSection, MovieDetail

--- movie-app/src/router/AppRouter.js
+++ movie-app/src/router/AppRouter.js
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Login from "../pages/Login";
 import Main from "../pages/Main";
 import Register from "../pages/Register";
+import MovieDetail from "../pages/MovieDetail";
 
 const AppRouter = () => {
   return (
@@ -12,6 +13,7 @@ const AppRouter = () => {
             <Route path="/" element={<Main />}/>
             <Route path="/login" element={<Login />}/>
             <Route path="/register" element={<Register />}/>
+            <Route path="/details/:id" element={<MovieDetail />}/>
         </Routes>
     </Router>
 )
